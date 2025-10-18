9cf13ec789be6ef28a43b73f719704b1c6fcc5ea
mangelryujin <mangelryujin@gmail.com>
2025-10-18 12:11:11 -0400

    config: remove server config

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,4 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), tailwindcss()],
-  server: {
-    port: 5173,
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 });
