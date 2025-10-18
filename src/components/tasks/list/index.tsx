c89024e8e5e05525b62852715ececc5a5351cc7d
mangelryujin <mangelryujin@gmail.com>
2025-10-18 11:57:57 -0400

    ref: modified color task

--- src/components/tasks/list/index.tsx
+++ src/components/tasks/list/index.tsx
@@ -9,7 +9,6 @@ export default function TasksList({tasks, onDelete}:TasksListProps){
             {tasks?.map((task,index)=>(
                 <Alert
                 key={index}
-                color="primary"
                 description={task.created_at}
                 endContent={
                 <Button onClick={()=> {onDelete(task.id)}} color="danger" size="sm" isIconOnly variant="flat">
