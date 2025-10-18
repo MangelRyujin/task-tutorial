bbb4ecdf23ed5dcb9520dd210414e10efdba8a24
mangelryujin <mangelryujin@gmail.com>
2025-10-18 12:09:40 -0400

    ref: change pt

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -8,7 +8,7 @@ export default function DefaultLayout({
   return (
     <div className="w-full relative flex flex-col">
       <Navbar />
-      <main className="container flex justify-center mx-auto max-w-7xl px-6 flex-grow pt-16">
+      <main className="container flex justify-center mx-auto max-w-7xl px-6 flex-grow pt-4">
         {children}
       </main>
     </div>
